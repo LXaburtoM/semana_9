a5304a443130bb19b78c748b30c7cb6d7e0e2e43
LXaburtoM <lxaburto@uam.edu.ni>
2025-05-20 11:28:13 -0600

    trabajo

---
+++ ejercicios_2.py
@@ -0,0 +1,8 @@
+# Solicita 10 números al usuario y calcula la suma total
+numeros = []
+for i in range(10):
+    num = int(input(f"Ingrese el número {i+1}: "))
+    numeros.append(num)
+
+suma_total = sum(numeros)
+print("La suma total es:", suma_total)
