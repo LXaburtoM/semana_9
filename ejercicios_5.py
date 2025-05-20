780bb30e650709ec916e9d7f853ad8b55f9b4f47
LXaburtoM <lxaburto@uam.edu.ni>
2025-05-20 11:34:45 -0600

    trabajo

---
+++ ejercicios_5.py
@@ -0,0 +1,5 @@
+# Lista de 10 números y contar cuántos son mayores que 50
+numeros = [45, 60, 72, 34, 99, 10, 50, 51, 77, 12]
+mayores_que_50 = [n for n in numeros if n > 50]
+print("Cantidad de números mayores que 50:", len(mayores_que_50))
+
